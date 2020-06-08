9dc3a9028ad424c2309774e3fb991e17a8a8bf5e
ido gueron <debigueron@Debis-MacBook-Air.local>
2020-06-08 12:04:18 +0300

    done

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -10,12 +10,30 @@ class Exercise1 extends Component {
         }
     }
 
+    updateName = (e) => {
+        this.setState({name:e.target.value})
+    }
+
+    updateAge = (e) => {
+        this.setState({age :e.target.value})
+    }
+    
+    handleChange = (e) => {
+        const key = e.target.name === 'name' ? 'name' : 'age'
+        const val = e.target.value
+        this.setState({[key]:val})
+    }
+
+    print = () => {
+        console.log(`${this.state.name} is ${this.state.age} years old and still rocking`)
+    }
+
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" name='name' value={this.state.name} onChange={this.handleChange}/>
+                <input id="age-input" name='age' value={this.state.age} onChange={this.handleChange}/>
+                <button onClick={this.print}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -10,11 +10,25 @@ constructor() {
     }
 }
 
+updateName = (event) => {
+    this.setState({name:event.target.value})
+}
+
+updateFruit = (event) => {
+    this.setState({fruit:event.target.value})
+}
+
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" value={this.state.name} onChange={this.updateName}/>
+            <select id="select-input" value={this.state.fruit} onChange={this.updateFruit} placeholder='Fruit'>
+                <option value="" disabled selected>Select your Fruit</option>
+                <option value="melon">Melon</option>
+                <option value="aplle">Apple</option>
+                <option value="bannana">Bannana</option>
+            </select>
+            <button onClick={() => console.log(this.state.name + ' has bought a ' + this.state.fruit)}>PICK</button>
         </div>
     );
 }
